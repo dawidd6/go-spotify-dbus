255c5ad689d0fae3a5da245ae20410e425ad4817
leo palmer sunmo <leo.sunmo@gmail.com>
2019-06-28 22:06:28 +1200

    Make a little spotify cli

--- examples/spotify-cli/main.go
+++ examples/spotify-cli/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
+	"time"
 
 	"github.com/godbus/dbus"
 	"github.com/leosunmo/go-spotify-dbus"
@@ -17,18 +19,51 @@ func main() {
 	switch action {
 	case "next":
 		err = spotify.SendNext(conn)
+		return
 	case "prev":
 		err = spotify.SendPrevious(conn)
+		return
 	case "play":
 		err = spotify.SendPlay(conn)
+		return
 	case "pause":
 		err = spotify.SendPause(conn)
-	case "":
-		err = spotify.SendPlayPause(conn)
+		return
 	}
 	if err != nil {
 		log.Fatalf("%s failed with err: %s", action, err.Error())
 	}
+	var ps spotify.PlaybackStatus
+	var meta *spotify.Metadata
+	ps, err = spotify.GetPlaybackStatus(conn)
+	if err != nil {
+		log.Fatalf("failed getting play/pause status, err: %s", err.Error())
+	}
+	meta, err = spotify.GetMetadata(conn)
+	if err != nil {
+		log.Fatalf("failed getting metadata, err: %s", err.Error())
+	}
+
+	switch action {
+	case "":
+		if ps == "Paused" {
+			fmt.Printf("▮▮ %s - %s\n", meta.Artist, meta.Title)
+		} else {
+			fmt.Printf("▶ %s - %s\n", meta.Artist, meta.Title)
+		}
+	case "info":
+		var ld time.Duration
+		l := int64(meta.Length)
+		ld = time.Duration(time.Duration(l) * time.Microsecond )
+		if ps == "Paused" {
+			fmt.Printf("▮▮ %s - %s\n\tAlbum: %s\n\tRating: %f\n\tLength: %v\n",
+				meta.Artist, meta.Title, meta.Album, meta.AutoRating, ld)
+		} else {
+			fmt.Printf("▶ %s - %s\n\tAlbum: %s\n\tRating: %f\n\tLength: %v\n",
+			meta.Artist, meta.Title, meta.Album, meta.AutoRating, ld)
+		}
+	}
+
 }
 
 func getConn() *dbus.Conn {
